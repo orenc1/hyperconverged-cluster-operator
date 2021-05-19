2129b3d3265ef6b7ecfa8e383ea6c8ed44d62d8c
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2021-05-19 13:02:34 +0300

    Enable eventual launcher updates by default (#1348)
    * Enable eventual launcher updates by default
    
    Add the workloadUpdateStrategy API to the HyperConverged CR. Propagate its fields to KubeVirt
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>
    
    * add some unit tests to increase coverage
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- pkg/webhooks/setup_test.go
+++ pkg/webhooks/setup_test.go
@@ -22,4 +22,19 @@ var _ = Describe("Hyperconverged API: Webhook", func() {
 			Expect(GetWebhookCertDir()).Should(Equal(somethingElse))
 		})
 	})
+
+	Context("Test GetWebhookCertDir", func() {
+		It("should return default value, if the env var is not set", func() {
+			Expect(GetWebhookCertDir()).Should(Equal(DefaultWebhookCertDir))
+		})
+
+		It("should return the value of the env var, if set", func() {
+			env := os.Getenv(webHookCertDirEnv)
+			defer os.Setenv(webHookCertDirEnv, env)
+
+			const somethingElse = "/something/else"
+			os.Setenv(webHookCertDirEnv, somethingElse)
+			Expect(GetWebhookCertDir()).Should(Equal(somethingElse))
+		})
+	})
 })
